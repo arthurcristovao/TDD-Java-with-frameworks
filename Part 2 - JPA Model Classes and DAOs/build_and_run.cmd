5c9e1c5b206fb4cf6c6698ff44b43ff4e5c2fadf
afss0 <125560943+afss0@users.noreply.github.com>
2024-07-01 19:22:22 -0300

    chore: Adapt build_and_run.cmd to the new changes in the project

--- Part 2 - JPA Model Classes and DAOs/build_and_run.cmd
+++ Part 2 - JPA Model Classes and DAOs/build_and_run.cmd
@@ -7,15 +7,17 @@ set "jarFile=target\taskapp.jar"
 
 setlocal enabledelayedexpansion
 
-:: Path to the .env file
-set "envFile=%~dp0.env"
+:: This step is not required anymore, since environment variables are now handled using dotenv-java, which will automatically read the .env file in the same folder where the built package resides.
 
-:: Read and set environment variables from .env
-for /F "tokens=*" %%a in (%envFile%) do (
-    for /F "tokens=1* delims==" %%b in ("%%a") do (
-        set %%b=%%c
-    )
-)
+:: Path to the .env file
+::set "envFile=%~dp0.env"
+
+:: Read and set environment variables from .env ()
+:: for /F "tokens=*" %%a in (%envFile%) do (
+::    for /F "tokens=1* delims==" %%b in ("%%a") do (
+::        set %%b=%%c
+::    )
+::)
 
 :: Execute the JAR file
 java -jar %jarFile%
